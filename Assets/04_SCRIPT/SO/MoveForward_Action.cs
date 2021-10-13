a441e7a85c76fa06df48f01fbf6c3d107d04e7c0
TiltGuy <raphael.gregori.gd@gmail.com>
2021-10-13 11:16:31 +0200

    Focus Behaviour Hiota (Passage en Français)
    - Hiota se tourne vers son ennemi même s'il ne se déplace pas
    - la camera se clampe sur la cible de hiota pour le suivre au-dessus de sa tête

--- Assets/04_SCRIPT/SO/MoveForward_Action.cs
+++ Assets/04_SCRIPT/SO/MoveForward_Action.cs
@@ -72,6 +72,19 @@ public class MoveForward_Action : Action_SO
             }
 
         }
+        else
+        {
+            if (controller.b_IsFocusing)
+            {
+                Vector3 hiotaPos = controller.transform.position;
+                Vector3 dir = (controller.currentHiotaTarget.position - hiotaPos).normalized;
+                dir.y = 0;
+                controller.directionToFocus = dir;
+                //Debug.DrawLine(hiotaPos, hiotaPos + dir * 10, Color.red, Mathf.Infinity);
+                Quaternion finalrot = Quaternion.LookRotation(controller.directionToFocus, Vector3.up);
+                controller.transform.rotation = Quaternion.Lerp(controller.transform.rotation, finalrot, controller.m_turnSpeed * Time.deltaTime);
+            }
+        }
 
         controller.Hiota_Anim.SetFloat("Input_Move_Vector", controller.m_InputMoveVector.magnitude);
 
