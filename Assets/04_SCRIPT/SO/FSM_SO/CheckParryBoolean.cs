78bb4b04f1ea5a07eb9032e2e3a0a2f0eaf3d58b
raphael GREGORI <raphael.gregori.gd@gmail.com>
2021-11-22 08:48:16 +0100

    Patch FSM Parry state : - Check Parry State debugged ==> Remove check Stun Boolean1 - Rework with the good namespace of CheckParryBoolean

--- Assets/04_SCRIPT/SO/FSM_SO/CheckParryBoolean.cs
+++ Assets/04_SCRIPT/SO/FSM_SO/CheckParryBoolean.cs
@@ -2,11 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(menuName = "PluggableStateMachine/Decisions/CheckStunBoolean_Decision")]
-public class CheckStunBoolean : Decision_SO
+[CreateAssetMenu(menuName = "PluggableStateMachine/Decisions/CheckParryBoolean_Decision")]
+public class CheckParryBoolean : Decision_SO
 {
     public override bool Decide(PlayerController_FSM controller)
     {
-        return controller.b_Stunned;
+        return controller.b_Parry;
     }
 }
