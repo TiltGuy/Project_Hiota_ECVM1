d3b3a37ced770b430c7d5759a38877add382330e
TiltGuy <raphael.gregori.gd@gmail.com>
2022-02-23 18:28:56 +0100

    Integration of Targetable Enemies in target Gatherer

--- Assets/TargetGatherer.cs
+++ Assets/TargetGatherer.cs
@@ -4,7 +4,12 @@ using UnityEngine;
 
 public class TargetGatherer : MonoBehaviour
 {
+    [SerializeField]
+    private Controller_FSM controller;
+    [SerializeField]
+    private LayerMask layerMask;
     public List<Transform> PotentialEnemies;
+    public List<Transform> TargetableEnemies;
 
     private void OnTriggerEnter(Collider other)
     {
@@ -24,7 +29,51 @@ public class TargetGatherer : MonoBehaviour
             if(PotentialEnemies.Contains(other.transform))
             {
                 PotentialEnemies.Remove(other.transform);
+                TargetableEnemies.Remove(other.transform);
             }
         }
+    }
+
+    private void Update()
+    {
+        if(PotentialEnemies.Count >0)
+        {
+            foreach (Transform enemies in PotentialEnemies)
+            {
+                Vector3 dir = (enemies.position - controller.eyes.position);
+                if(CheckSightLine(enemies.transform))
+                {
+                    Debug.DrawRay(controller.eyes.position, dir, Color.yellow);
+                    // if True then add to another List of Targetable Enemies
+                    if(!TargetableEnemies.Contains(enemies.transform))
+                    {
+                        TargetableEnemies.Add(enemies.transform);
+                    }
+                }
+                else
+                {
+                    if (TargetableEnemies.Contains(enemies.transform))
+                    {
+                        TargetableEnemies.Remove(enemies.transform);
+                    }
+                    // if Not then remove this element of the List of Targetable Enemies
+                }
+            }
+        }
+    }
+
+    private bool CheckSightLine(Transform target)
+    {
+        RaycastHit hit;
+        Vector3 dir = (target.position - controller.eyes.position);
+        if (Physics.Raycast(controller.eyes.position, dir, out hit, 1000, layerMask))
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+
     }
 }
