a79966ec973fefca1ccada77f3977bf7b88cd427
LeroyFel <felix.leroy@mail-ecv.fr>
2022-02-28 14:45:16 +0100

    Updated Broken_Guard FX Incoming attack FX Enemy Sword Slash FX W.I.P. of Enemy Stopping Chase FX of Weapon change First Draft to change Material upon dying

---
+++ Assets/__TESTASSETS/F_Leroy/F_07_TestScripts/Death_Color_Change.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Death_Color_Change : MonoBehaviour
+{
+    public Material[] material;
+    public int x;
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        x=0;
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[x];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.sharedMaterial = material[x];
+        
+        if (Input.GetKey("r"))
+            {
+              x++;
+            }
+    }
+
+   
+
+}
