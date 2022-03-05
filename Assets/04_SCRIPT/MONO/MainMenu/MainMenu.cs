2f712ff05a22344d2607d2ac483a68c2c05bea0b
CeliaLet <78477658+CeliaLet@users.noreply.github.com>
2022-03-05 13:28:04 +0100

    Made a main menu (start settings quit)

---
+++ Assets/04_SCRIPT/MONO/MainMenu/MainMenu.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+	public string firstLevel;
+
+	public GameObject pauseMenu, optionsWindow;
+
+
+	/*private void Update()
+	{
+		if (Input.GetButtonDown("Fire3"))
+		{
+			PauseUnpause();
+		}
+	}
+
+	public void PauseUnpause()
+	{
+		if (!pauseMenu.activeInHierarchy)
+		{
+			pauseMenu.SetActive(true);
+			Time.timeScale = 0f;
+		}
+		else
+		{
+			pauseMenu.SetActive(false);
+			Time.timeScale = 1f;
+			optionsWindow.SetActive(false);
+		}
+	}*/
+	public void StartGame()
+	{
+		SceneManager.LoadScene(firstLevel);
+	}
+
+	public void OpenOptions()
+	{
+		optionsWindow.SetActive(true);
+	}
+
+	public void CloseOptions()
+	{
+		optionsWindow.SetActive(false);
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+		Debug.Log("Quit");
+	}
+
+	
+}
