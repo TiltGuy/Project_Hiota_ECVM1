e505381691f80e62f2c835bdddb70cec4496c216
ADE\cletrun_ecv <letruncelia@gmail.com>
2022-10-25 14:28:20 +0200

    Celia Test Assets : created LD scene with blockout and animations

---
+++ Assets/__TESTASSETS/cletrun/Scripts/NarrativeZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NarrativeZone : MonoBehaviour
+{
+    public Animator animator;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator.SetBool("NarrativeZone", false);
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            animator.SetBool("NarrativeZone", true);
+        }
+
+    }
+}
