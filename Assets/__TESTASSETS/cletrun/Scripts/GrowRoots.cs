d4f6650ea52db66b91655987d62cbb70c83f4b46
ADE\cletrun_ecv <letruncelia@gmail.com>
2022-11-16 16:41:05 +0100

    Blockout Scene = Growing bridge for changing lvl

--- Assets/__TESTASSETS/cletrun/Scripts/GrowRoots.cs
+++ Assets/__TESTASSETS/cletrun/Scripts/GrowRoots.cs
@@ -10,15 +10,14 @@ public class GrowRoots : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
-    {
+    {    
         animator.SetBool("Grow", false);
-
     }   
 
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {
+        {           
             animator.SetBool("Grow", true);
         }
         
