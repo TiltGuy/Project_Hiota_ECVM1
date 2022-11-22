f82c5157b408265a7843426df53daebe88c900e2
ADE\fleroy_ecv <felix@leroy@mail-ecv.fr>
2022-11-22 17:06:27 +0100

    Tweaks FX and Putting the old ones in a subfolder

---
+++ Assets/__TESTASSETS/F_Leroy/F_07_TestScripts/SwordVisualEffects.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwordVisualEffects : MonoBehaviour
+{
+
+    public Transform ps;
+    public Animator animator;
+
+    public void WindUpGlow()
+    {
+        Instantiate(ps, transform.position, Quaternion.identity);
+    }
+
+}
