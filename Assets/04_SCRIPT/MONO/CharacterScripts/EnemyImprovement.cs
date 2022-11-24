392381e8990a92b64aac8562491b088f4cc77d3f
ADE\rgregor_ecv <raphael.gregori.gd@gmail.com>
2022-11-24 16:55:20 +0100

    Spawn Skill Card Inplemented + add Skill Card

--- Assets/04_SCRIPT/MONO/CharacterScripts/EnemyImprovement.cs
+++ Assets/04_SCRIPT/MONO/CharacterScripts/EnemyImprovement.cs
@@ -5,31 +5,47 @@ using UnityEngine.Events;
 
 public class EnemyImprovement : MonoBehaviour, IDamageable
 {
+    private Animator _animator;
     public List<string> TargetTags = new List<string>();
     public string EnemyTag;
     public List<Transform> Enemies = new List<Transform>();
     public UnityEvent OnSelectSkillCard;
     private void Awake()
     {
+        UpdateListOfEnemies();
+        _animator = GetComponent<Animator>();
+    }
+
+    private void UpdateListOfEnemies()
+    {
         foreach ( GameObject enemy in GameObject.FindGameObjectsWithTag(EnemyTag) )
         {
             Enemies.Add(enemy.GetComponent<Transform>());
         }
     }
+
     private void Start()
     {
-        
+        //AssignNewSkillCard();
+    }
+
+    private void AssignNewSkillCard()
+    {
         foreach ( Transform enemy in Enemies )
         {
-            enemy.gameObject.AddComponent<SkillCardScript>();
+            if(enemy.gameObject.activeInHierarchy)
+            {
+                enemy.gameObject.AddComponent<SkillCardScript>();
+            }
         }
     }
 
     public void TakeDamages( float damageTaken, Transform Striker, bool isAHook )
     {
-        foreach( GameObject enemy in GameObject.FindGameObjectsWithTag(EnemyTag))
-        {
-            //Enemies.Add(enemy.GetComponent<Controller_FSM>());
-        }
+        print("Take FUCKING Damages");
+        AssignNewSkillCard();
+        _animator.SetTrigger("t_Activated");
+        OnSelectSkillCard?.Invoke();
     }
+
 }
