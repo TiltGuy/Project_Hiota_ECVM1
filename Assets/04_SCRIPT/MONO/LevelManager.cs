87fe50d79a399376f47c72735d1319181bb95084
ADE\rgregor_ecv <raphael.gregori.gd@gmail.com>
2023-01-04 17:13:09 +0100

    Push spawning ennemies in Random way

---
+++ Assets/04_SCRIPT/MONO/LevelManager.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    public static float currentRoomIndex;
+
+    [SerializeField]
+    private GameObject[] Props;
+
+    [SerializeField]
+    private GameObject[] Troups;
+    [SerializeField]
+    private float nbTotalRooms;
+
+    [SerializeField]
+    private float minProbability = 25;
+    [SerializeField]
+    private float maxProbability = 75;
+
+    private void Start()
+    {
+        currentRoomIndex = 1;
+        //foreach (GameObject prop in Props)
+        //{
+        //    if (Random.Range(0,100) > minProbability + currentRoomIndex/nbTotalRooms * (maxProbability - minProbability) )
+        //    {
+        //        prop.SetActive(true);
+        //    }
+        //}
+
+        int nbToBeSpawned = Mathf.FloorToInt(1 + currentRoomIndex / nbTotalRooms * (Props.Length - 1));
+        Debug.Log(nbToBeSpawned);
+        for (int i = 0; i < nbToBeSpawned; i++)
+        {
+            int security = 0;
+            int randomIndex = Random.Range(0, Props.Length);
+            //Debug.Log("1er Random Index = " + randomIndex);
+            while (Props[randomIndex].activeInHierarchy)
+            {
+                randomIndex = Random.Range(0, Props.Length);
+                //Debug.Log("2ème Random Index = " + randomIndex);
+                security++;
+                if(security>1000)
+                {
+                    Debug.LogError("Boucle fini sale vilain !!! ", this);
+                    break;
+                }
+            }
+            Props[randomIndex].SetActive(true);
+        }
+    }
+}
---
+++ Assets/__TESTASSETS/rgregori/01_SCRIPT/MONO/SpawnTroup.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnTroup : MonoBehaviour
+{
+
+    public Troup_SO[] Troups;
+    public float distToSpawn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(Troups != null)
+        {
+            int troopIndex = Random.Range(0, Troups.Length - 1);
+            foreach ( GameObject enemy in Troups[troopIndex].Enemies )
+            {
+                Vector3 randomPosition = new Vector3( Random.insideUnitCircle.x * distToSpawn, transform.position.y, Random.insideUnitCircle.y * distToSpawn);
+                Instantiate(enemy, randomPosition, Quaternion.identity);
+            }
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, distToSpawn);
+    }
+}
