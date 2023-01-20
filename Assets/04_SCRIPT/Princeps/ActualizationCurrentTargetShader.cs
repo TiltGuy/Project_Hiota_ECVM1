345db4f8f95ab50a1e75a8df47fccb0752beab55
ADE\rgregor_ecv <raphael.gregori.gd@gmail.com>
2023-01-20 12:02:37 +0100

    Begin coding script that remove or add the midifications on the target (SHADER + position flèche)

---
+++ Assets/04_SCRIPT/Princeps/ActualizationCurrentTargetShader.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActualizationCurrentTargetShader : MonoBehaviour
+{
+    private void NewTarget(GameObject NewTarget_go)
+    {
+        // Change le shader 
+        // Change la flèche de position
+    }
+
+    private void RemoveTarget( GameObject NewTarget_go )
+    {
+
+    }
+}
