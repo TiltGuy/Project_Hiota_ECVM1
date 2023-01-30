88636fa02ce4fe530cc2e25522eff72400abcd04
ADE\rgregor_ecv <raphael.gregori.gd@gmail.com>
2023-01-30 16:15:37 +0100

    Change the List of build Index Scenes

--- Assets/04_SCRIPT/Princeps/SO/ScenesNames.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(menuName = "List of Scene Names")]
-public class ScenesNames : ScriptableObject
-{
-    public string[] ListOfScenes;
-}
