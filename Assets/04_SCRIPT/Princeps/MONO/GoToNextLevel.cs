c31685cdba5a0a65ea6ad208c375ba17f790bbcc
TiltGuy <raphael.gregori.gd@gmail.com>
2023-01-31 14:35:21 +0100

    Removed useless variables

--- Assets/04_SCRIPT/Princeps/MONO/GoToNextLevel.cs
+++ Assets/04_SCRIPT/Princeps/MONO/GoToNextLevel.cs
@@ -4,9 +4,6 @@ using UnityEngine;
 
 public class GoToNextLevel : MonoBehaviour
 {
-    public bool b_SaveTutoState = false;
-    public bool b_HaveToSave = false;
-
     private void OnTriggerEnter( Collider other )
     {
         if(other.tag == "Player")
