639238b5c25b2f5c017220dd75d1724a83f00e8d
TiltGuy <raphael.gregori.gd@gmail.com>
2023-03-10 12:27:24 +0100

    begining of inplmentation of the explosion card

---
+++ Assets/04_SCRIPT/Princeps/SO/__BASE_SKILLCARD_SO/SCE_Explosion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "EFCT_SO",
+    menuName = "SkillCard/SCE_Explosion")]
+
+public class SCE_Explosion : Effect_SO
+{
+    public GameObject explosionPrefab;
+
+    public override void AddEffect( Controller_FSM controller, CharacterSpecs characterSpecs )
+    {
+        // Do your stuff
+        characterSpecs.onHealthDepleted += SpawnExplosion;
+    }
+
+    private void SpawnExplosion( CharacterSpecs characterSpecs )
+    {
+        GameObject gameObject = Instantiate(explosionPrefab,
+            characterSpecs.transform.position,
+            characterSpecs.transform.rotation);
+    }
+}
