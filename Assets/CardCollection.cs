48fac2664922fe46b036c699ca86f979ed1c460c
ADE\rgregor_ecv <raphael.gregori.gd@gmail.com>
2023-03-16 16:34:12 +0100

    Start of a Collection System

---
+++ Assets/CardCollection.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CardCollection : MonoBehaviour
+{
+    public TMP_Text Title_Text;
+    public TMP_Text Bonus_Texts;
+    public TMP_Text Malus_Texts;
+    public string bonusPrefix = "Bonus";
+    public string malusPrefix = "Malus";
+    public string linePrefix = "> ";
+    public SkillCard_SO currentSkillcard;
+    public Collection refCollection;
+    public GameObject feedbackSelectedImage;
+
+    public bool b_IsSelected;
+
+    public void AssignText( SkillCard_SO skillCard )
+    {
+        currentSkillcard = skillCard;
+        UpdateCardMessages(Bonus_Texts, skillCard, true);
+        UpdateCardMessages(Malus_Texts, skillCard, false);
+        UpdateCardMessageTitle(Title_Text, skillCard);
+    }
+
+    private void UpdateCardMessages( TMP_Text textToUpdate, SkillCard_SO skillCard, bool b_BonusDisplay )
+    {
+        string newLine = System.Environment.NewLine;
+        if ( b_BonusDisplay )
+        {
+            if ( skillCard.Bonus.Count > 0 )
+            {
+                if ( !string.IsNullOrEmpty(bonusPrefix) )
+                    textToUpdate.text = bonusPrefix + "\n";
+                for ( int j = 0; j < skillCard.Bonus.Count; j++ )
+                {
+                    // first Line of Text
+                    if ( j == 0 )
+                    {
+                        textToUpdate.text += linePrefix + skillCard.Bonus[j].cardMessage;
+                    }
+                    else
+                    {
+                        textToUpdate.text += newLine + linePrefix + skillCard.Bonus[j].cardMessage;
+                    }
+                }
+            }
+        }
+        else
+        {
+            if ( skillCard.Malus.Count > 0 )
+            {
+                if ( !string.IsNullOrEmpty(malusPrefix) )
+                    textToUpdate.text = malusPrefix + "\n";
+                for ( int j = 0; j < skillCard.Malus.Count; j++ )
+                {
+                    // first Line of Text
+                    if ( j == 0 )
+                    {
+                        textToUpdate.text += linePrefix + skillCard.Malus[j].cardMessage;
+                    }
+                    else
+                    {
+                        textToUpdate.text += newLine + linePrefix + skillCard.Malus[j].cardMessage;
+                    }
+                }
+            }
+        }
+
+    }
+
+    private void UpdateCardMessageTitle( TMP_Text textToUpdate, SkillCard_SO skillCard )
+    {
+        textToUpdate.text = skillCard.cardName;
+
+    }
+
+    public void SetFeedbackSelected(bool value)
+    {
+        if(value)
+        {
+            b_IsSelected = true;
+            feedbackSelectedImage.SetActive(true);
+        }
+        else
+        {
+            b_IsSelected =false;
+            feedbackSelectedImage.SetActive(false);
+        }
+        
+    }
+
+    public void ButtonClick()
+    {
+        refCollection.ToggleCardToDeck(this);
+    }
+}
---
+++ Assets/Collection.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using System.Linq;
+
+public class Collection : MonoBehaviour
+{
+    public Canvas cardCanvas;
+    public ListOfCards collection;
+    public GameObject prefabCard;
+    public GameObject panelDisplay;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateDisplay();
+    }
+
+
+    private void UpdateDisplay()
+    {
+        foreach (SkillCard_SO card in collection.ListCards)
+        {
+            GameObject clone = Instantiate(prefabCard, panelDisplay.transform);
+            CardCollection cardScript = clone.GetComponent<CardCollection>();
+            cardScript.AssignText(card);
+            cardScript.refCollection = this;
+            if(DeckManager.instance._PlayerDeck.Contains(card))
+            {
+                cardScript.SetFeedbackSelected(true);
+            }
+        }
+        cardCanvas.GetComponentsInChildren<Selectable>().First().Select();
+    }
+
+    public void ToggleCardToDeck(CardCollection card)
+    {
+        if(card.b_IsSelected)
+        {
+            DeckManager.instance._PlayerDeck.Remove(card.currentSkillcard);
+            card.SetFeedbackSelected(false);
+        }
+        else
+        {
+            if ( DeckManager.instance._PlayerDeck.Count < 24 )
+            {
+                DeckManager.instance._PlayerDeck.Add(card.currentSkillcard);
+                card.SetFeedbackSelected(true);
+
+            }
+        }
+    }
+
+    
+}
