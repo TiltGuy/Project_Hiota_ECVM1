974df7e9bbcc436807b42a96ba8e5732545e0c81
CeliaLet <letruncelia@gmail.com>
2023-05-05 10:51:10 +0200

    Cards Anim got Sound FX

---
+++ Assets/playSoundCard.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playSoundCard : MonoBehaviour
+{
+    AudioSource soundFX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        soundFX = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void playSoundFX()
+    {
+        soundFX.Play();
+    }
+}
